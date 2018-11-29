e2f7192dcf94b3e3a0e04de02db643eb3ec0925c
ahmedfmmurad <ahmadfayez93@gmail.com>
2018-11-29 18:07:21 -0500

    Express added, ready for heroku

--- server/server.js
+++ server/server.js
@@ -1,1 +1,13 @@
-console.log("working?");
+const express = require('express');
+const path = require('path');
+app = express();
+
+const www = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(www));
+
+
+app.listen(port, () => {
+    console.log(`Server is up at ${port}`)
+});
