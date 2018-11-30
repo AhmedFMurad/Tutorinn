844df826bb0da7ab9db7524d2b29f4ffc2cdac5b
ahmedfmmurad <ahmadfayez93@gmail.com>
2018-11-30 15:08:05 -0500

    Socket fix, user connections registering

--- server/server.js
+++ server/server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const path = require('path');
 const socketIO = require('socket.io');
 const http = require('http');
+
+
 var app = express(); 
-var server = http.createServer(http);
+var server = http.createServer(app);
 var io = socketIO(server);
 const www = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(www));
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
     console.log("User connected");
 });
 
